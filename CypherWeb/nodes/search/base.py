eb65413cc608872b9bfaa7b7b4c612f4fb31cb34
AnasAito <khdimaty@gmail.com>
2023-09-15 12:25:09 +0100

    tweak distance elem to anchor

--- CypherWeb/nodes/search/base.py
+++ CypherWeb/nodes/search/base.py
@@ -15,8 +15,16 @@ class GraphNearestNeighbor(Node):
     def score_pairs_of_nodes(self, graph, node_ref, node):
         ancestor = graph.lowest_common_ancestor(node_ref, node)
         if ancestor == node:  # Note : nodes within grid will return 0
-            return self.get_distance(graph, node_ref, ancestor)
-        return self.get_distance(graph, node, ancestor)
+            return {
+                "dist": self.get_distance(graph, node_ref, ancestor),
+                "anchor_dist": self.get_distance(graph, node_ref, ancestor),
+            }
+        ances_node_dist = self.get_distance(graph, node, ancestor)
+        ances_ref_dist = self.get_distance(graph, node_ref, ancestor)
+        return {
+            "dist": max(ances_node_dist, ances_ref_dist),
+            "anchor_dist": ances_ref_dist,
+        }
 
     def run(self, payload: dict, params: dict = None) -> dict:
         if "str_matcher" in payload:
@@ -32,21 +40,26 @@ class GraphNearestNeighbor(Node):
                 results = {}
                 for grid_node in cand_nodes:
                     # print(grid_node)
-                    distance = self.score_pairs_of_nodes(graph, anchor_node, grid_node)
-                    # print(distance)
+                    distance_meta = self.score_pairs_of_nodes(
+                        graph, anchor_node, grid_node
+                    )
+                    # print(distance_meta)
                     # if (
                     #     distance != 0
                     # ):  # TODO we should solve why main node is a grid ? then we can activate again
-                    results[grid_node] = distance
+                    results[grid_node] = distance_meta
                     # print("-------------------")
                 # sort by distance
-                results = sorted(results.items(), key=lambda item: item[1])
+                results = sorted(
+                    results.items(),
+                    key=lambda item: (item[1]["anchor_dist"], item[1]["dist"]),
+                )
                 # get min dist
-                min_dist = results[0][1]
+                min_dist = results[0][1]["dist"]
                 best_grids = [
                     {"node": node, "distance": distance}
                     for node, distance in results
-                    if distance == min_dist
+                    if distance["dist"] == min_dist
                 ]
                 output[anchor_node] = best_grids
             return {f"{node_type}_results": output}
